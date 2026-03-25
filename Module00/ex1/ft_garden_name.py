66c464ee1a94ed265eb10a74be67b2ac23f82982
raqroca- <raqroca-@student.42madrid.com>
2026-03-25 18:04:32 +0100

    ex7

--- Module00/ex1/ft_garden_name.py
+++ Module00/ex1/ft_garden_name.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
 def ft_garden_name() -> None:
-    name: str = input(("Enter garden name: "))
+    name: str = input("Enter garden name: ")
     print(f"Garden: {name}")
     print("Status: Growing well!")
