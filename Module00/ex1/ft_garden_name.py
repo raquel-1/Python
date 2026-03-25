ac99f7848989bcfab464d7f8f2d76a234bd67e87
raqroca- <raqroca-@student.42madrid.com>
2026-03-25 15:49:31 +0100

    ex2

--- Module00/ex1/ft_garden_name.py
+++ Module00/ex1/ft_garden_name.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 
-def ft_garden_name() -> None :
-	
+def ft_garden_name() -> None:
+    name: str = input(("Enter garden name: "))
+    print(f"Garden: {name}")
+    print("Status: Growing well!")
---
+++ Module00/ex2/ft_plot_area.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def ft_plot_area() -> None:
+    l: int = int(input("Enter length: "))
+    w: int = int(input("Enter width: "))
+    print(f"Plot area: {l * w}")
