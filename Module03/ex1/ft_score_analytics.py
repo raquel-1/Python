9778760eb11bfa2403e1f78414fce7c69a8a90da
raqroca- <raqroca-@student.42madrid.com>
2026-04-10 16:41:20 +0200

    ex3 ..

--- Module03/ex1/ft_score_analytics.py
+++ Module03/ex1/ft_score_analytics.py
@@ -12,12 +12,25 @@ if __name__ == "__main__":
     else:
         lis = sys.argv[1:]
         bad = 0
+        sum = 0
+        high = 0
         for i in lis:
             try:
-                input_argv(i)
+                n = input_argv(i)
+                sum += n
             except Exception as e:
                 print(f"Invalid parameter: '{i}'")
                 bad = 1
         if (bad != 0):
             print("No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ...")
-
+        else:
+            lis_int = [0] * len(lis)
+            for i in range(len(lis)):
+                lis_int[i] = input_argv(lis[i])
+            print("Scores processed:", lis_int)
+            print("Total players:", len(lis))
+            print("Total score:", sum)
+            print("Average score:", sum / len(lis))
+            print("High score:", max(lis_int))
+            print("Low score:", min(lis_int))
+            print("Score range:", max(lis_int) - min(lis_int))
