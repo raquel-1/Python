4343fcb5ae6ee406361a35dc84e8653b799109e1
raquel-1 <rocamartinezraquel@gmail.com>
2026-05-22 23:39:54 +0200

    ex1 enum

--- Module09/ex1/alien_contact.py
+++ Module09/ex1/alien_contact.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python3
-
+from enum import Enum
 from typing import Optional
 from datetime import datetime
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
+
+
+class ContactType(str, Enum):
+    radio = "radio"
+    visual = "visual"
+    physical = "physical"
+    telepathic = "telepathic"
+
+
+class AlienContact(BaseModel):
+    contact_id: str = Field(min_length=5, max_length=15)
+    timestamp: datetime
+    location: str = Field(min_length=3, max_length=100)
+    contact_type: ContactType
+    signal_strength: float = Field(ge=0.0, le=10.0)
+    duration_minutes: int = Field(ge=1, le=1440)
+    witness_count: int = Field(ge=1, le=100)
+    message_received: Optional[str] = Field(default=None, max_length=500)
+    is_verified: bool = Field(default=False)
+
+    @model_validator(mode='after')
+    def my_validator(self) -> 'MiModelo':
+        # aquí tienes acceso a self.campo1, self.campo2...
+        # si algo falla lanzas un ValueError
+        return self  # siempre al final
 
 
 class SpaceStation(BaseModel):
